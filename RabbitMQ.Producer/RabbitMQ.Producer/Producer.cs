69e2c3920e99dcbc333fca36cc0444dbf8e011a1
Sergey Korenkov <3ug3ag@gmail.com>
2012-10-23 21:52:24 +0300

    add

---
+++ RabbitMQ.Producer/RabbitMQ.Producer/Producer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMQ.Producer
+{
+    class Start
+    {
+        public static void Main()
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("start", false, false, false, null);
+
+                string message = "!";
+                byte[] body = System.Text.Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", "start", null, body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+        }
+
+    }
+}
